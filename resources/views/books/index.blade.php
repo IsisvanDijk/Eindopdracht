f2ae271874bd2f47780c8e9e3f3c4726d68bce80
IsisvanDijk <144005645+IsisvanDijk@users.noreply.github.com>
2024-10-15 12:23:13 +0200

    changed the index page spacing

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,10 +5,13 @@
         </h2>
     </x-slot>
 
-    @foreach ($books as $book)
-        <a href="{{ route('books.show', $book->id) }}">
-            <div class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+
+            <div class="py-10">
+
+                @foreach ($books as $book)
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-4">
+                    <a href="{{ route('books.show', $book->id) }}">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900 dark:text-gray-100">
 
@@ -19,8 +22,9 @@
                             </tr>
                         </div>
                     </div>
+                    </a>
                 </div>
+                @endforeach
             </div>
-        </a>
-    @endforeach
+
 </x-app-layout>
